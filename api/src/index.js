7373d2850870527c2113bbafe5171d2303b70d99
Leejaeyeop <dlwoduq78@naver.com>
2023-03-27 22:58:33 +0900

    createStory, createImg, createPdf 함수 구현 및 pdfkit 라이브러리 추가
    - createStory 함수 구현
    - createImg 함수 구현
    - createPdf 함수 구현
    - pdfkit 라이브러리 추가

--- api/src/index.js
+++ api/src/index.js
@@ -1,24 +1,109 @@
 require('dotenv').config();
+const express = require('express');
+const app = express()
 const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+const pdfDocumnet = require('pdfkit');
+const fs = require('fs');
+const https = require('https');
 
-async function gptTest() {
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY
-      });
-      const openai = new OpenAIApi(configuration);
-      
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-        temperature: 0,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["\n"],
-      });
-
-      console.log(response.data.choices)
+// sample data
+let texts = []
+let imgs = []
+let doc = null
+async function createStory() {    
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Please write a fairy tale book with the title of the book called 'Lee Jae-yeop'. The topic is about adventure.",
+    temperature: 0,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    // stop: [" "],
+  });
+  let text = response.data.choices[0].text
+  texts = text.split("\n")
+  texts = texts.filter(text => text !== '')
+  console.log(texts)
 }
 
-gptTest()
+async function createImg() {
+  // for(let text of texts[0]) {
+  //   const response = await openai.createImage({
+  //     prompt: text,
+  //     n: 1,
+  //     size: "128x128",
+  //   });
+  //   imgs.push(response.data[0].url)
+  // }
+  // console.log(texts[0])
+  const response = await openai.createImage({
+    prompt: texts[0],
+    n: 1,
+    size: "256x256",
+  });
+  imgs.push(response.data.data[0].url)
+}
+
+function initPdf() {
+  doc = new pdfDocumnet();
+  doc.pipe(fs.createWriteStream("output.pdf"));
+  // doc.font("NanumGothic.ttf");
+
+}
+
+function completePdf() {
+  doc.end()
+}
+
+async function createPdf() {
+  for(let i = 0; i<texts.length; i++) {
+    let text = texts[i]
+    let imgUrl = imgs[i]
+    console.log("시작")
+    https.get(imgUrl, (response) => {
+      console.log(response)
+      const chunks = [];
+    
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+    
+      response.on('end', () => {
+        const buffer = Buffer.concat(chunks);
+        const doc = new pdfDocumnet();
+        // doc.pipe(fs.createWriteStream("output.pdf"));
+        console.log("삽입")
+        doc.image(buffer).text(text);
+      });
+    }).on('error', (error) => {
+      console.error(error);
+    });
+  }
+} 
+
+// server open
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+async function main() {
+  // pdf 파일 생성
+  initPdf()
+  // story를 만든다.
+  await createStory()
+  // img를 만든다.
+  await createImg()
+  // pdf를 만든다.
+  await createPdf()
+  // pdf 파일 제작 완료
+  completePdf()
+}
+
+
+main()
+// createImg()
