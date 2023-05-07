7be41059788310f344a2bbf11eabf851a525ed0e
Leejaeyeop <dlwoduq78@naver.com>
2023-05-07 21:48:43 +0900

    api 변경
    - font 추가
    - title과 strory 생성 api 분리
    - 전역 변수를 로컬 변수로

--- api/src/index.js
+++ api/src/index.js
@@ -4,26 +4,40 @@ import { Configuration, OpenAIApi } from "openai";
 import pdfDocumnet from "PDFKit";
 import fs from "fs";
 import https from "https";
-import pdfjsLib from "pdfjs-dist";
 import cors from "cors";
+import bodyParser from "body-parser";
+import path from "path";
 dotenv.config();
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-// const pdfDocumnet = require('pdfkit');
 const app = express();
 const PORT = 3000;
 
 // sample data
-let texts = [];
 let imgs = [];
-let doc = null;
-async function createStory() {
-    texts = [];
+
+app.use(bodyParser.json());
+
+async function createTitles(data) {
+    console.log("createTitle!");
+    let texts = [];
+    let genre = data.genre;
+    if (genre !== "아무거나") {
+        genre = ", 주제가 " + genre;
+    } else {
+        genre = "";
+    }
+
+    let prompt =
+        "주인공이 " + data.mainCharacter + genre + ", 동화 이야기를 제목과 간략한 줄거리 5개 정도 추천해 주세요.";
+
+    console.log(prompt);
+
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Please write a fairy tale book with the title of the book called 'Lee Jae-yeop'. The topic is about adventure.",
+        prompt: prompt,
         temperature: 0,
         max_tokens: 2048,
         top_p: 1,
@@ -35,9 +49,61 @@ async function createStory() {
     texts = text.split("\n");
     texts = texts.filter((text) => text !== "");
     console.log(texts);
+
+    return texts;
 }
 
-async function createImg() {
+async function createStory(title) {
+    console.log("createStory!");
+    let prompt =
+        title +
+        "\n 다음 내용으로 동화 이야기를 만들어 주세요. \n 이야기는 8개의 단락 으로 만들어 주세요. \n 영어와 한글로 각각 만들어 주세요.";
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        // stop: [" "],
+    });
+    let text = response.data.choices[0].text;
+
+    let textSplited = [];
+    textSplited = text.split("\n");
+    textSplited = textSplited.filter((text) => text !== "");
+
+    let texts = { kor: [], eng: [] };
+    for (let text of textSplited) {
+        console.log(text);
+        if (text.length < 10) continue;
+
+        if (checkLanguage(text) === "English") {
+            texts.eng.push(text);
+        } else if (checkLanguage(text) === "Korean") {
+            texts.kor.push(text);
+        }
+    }
+
+    console.log(texts);
+    return texts;
+}
+
+function checkLanguage(str) {
+    const koreanRegex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/;
+    const englishRegex = /[a-zA-Z]/;
+
+    if (koreanRegex.test(str)) {
+        return "Korean";
+    } else if (englishRegex.test(str)) {
+        return "English";
+    } else {
+        return "Other";
+    }
+}
+
+async function createImg(texts) {
     imgs = [];
     for (let text of texts) {
         const response = await openai.createImage({
@@ -47,26 +113,22 @@ async function createImg() {
         });
         imgs.push(response.data.data[0].url);
     }
-    // console.log(texts[0])
-    // const response = await openai.createImage({
-    //   prompt: texts[0],
-    //   n: 1,
-    //   size: "256x256",
-    // });
     console.log(imgs);
+    return imgs;
 }
 
+const __dirname = path.resolve();
 function initPdf() {
-    doc = new pdfDocumnet();
-    // doc.font("NanumGothic.ttf");
+    const dir = path.join(__dirname, "/resources/fonts/InkLipquidFonts.ttf");
+    const doc = new pdfDocumnet();
+    const fontBytes = fs.readFileSync(dir);
+    doc.registerFont("InkLipquid", fontBytes);
+    doc.font("InkLipquid").fontSize(28);
+
+    return doc;
 }
 
-// function completePdf() {
-//   doc.end()
-//   console.log("end")
-// }
-
-async function createPdf() {
+async function createPdf(doc, texts, imgs) {
     const imgPromises = [];
     for (let i = 0; i < imgs.length; i++) {
         let text = texts[i];
@@ -97,21 +159,12 @@ async function createPdf() {
         doc.image(buffer).text(text);
     }
 
+    doc.addPage();
+    doc.text("- 이야기 끝 -");
+
     doc.pipe(fs.createWriteStream("output.pdf"));
-    // doc.end();
 
     console.log("종료");
-}
-
-async function main() {
-    // pdf 파일 생성
-    initPdf();
-    // story를 만든다.
-    await createStory();
-    // img를 만든다.
-    await createImg();
-    // pdf를 만든다.
-    await createPdf();
 }
 
 app.use(
@@ -127,16 +180,22 @@ app.listen(PORT, function () {
     // main();
 });
 
-app.get("/api/books", async (req, res) => {
+app.post("/api/books", async (req, res) => {
     console.log("init making a book!");
+
+    let title = req.body.title;
+
+    console.log(title);
     // pdf 파일 생성
-    initPdf();
+    const doc = initPdf();
     // story를 만든다.
-    await createStory();
+    let texts = await createStory(title);
+    // Todo texts 를 영어로 변환
+
     // img를 만든다.
-    await createImg();
+    let imgs = await createImg(texts.eng);
     // pdf를 만든다.
-    await createPdf();
+    await createPdf(doc, texts.kor, imgs);
 
     // Set HTTP response headers
     res.setHeader("Content-Type", "application/pdf");
@@ -146,29 +205,14 @@ app.get("/api/books", async (req, res) => {
     doc.end();
 });
 
-// function pdfParse() {
-
-//   const pdfPath = 'output.pdf'
-//   const loadingTask = pdfjsLib.getDocument(pdfPath);
-
-//   loadingTask.promise.then( pdf => {
-//     const numPages = pdf.numPages
-//     for(let i =1; i<=numPages; i++) {
-//       pdf.getPage(i).then(page => {
-//         const viewport = page.getViewport({ scale:1 });
-//         const canvas = document.createElement('canvas')
-//         const canvasContext = canvas.getContext('2d')
-//         canvas.height = viewport.height
-//         canvas.width = viewport.width
-
-//         const renderTask = page.render({
-//           canvasContext,
-//           viewport
-//         })
-
-//       })
-//     }
-//   })
-// }
-
-// pdfParse()
+app.post("/api/title", async (req, res) => {
+    try {
+        let data = req.body.data;
+        console.log(data);
+        // title를 만든다.
+        let texts = await createTitles(data);
+        res.json(texts);
+    } catch (error) {
+        res.send(error);
+    }
+});
