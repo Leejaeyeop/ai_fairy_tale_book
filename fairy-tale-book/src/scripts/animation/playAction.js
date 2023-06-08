252f690e0c2738116e954a4f58cdac87e66240bc
Leejaeyeop <dlwoduq78@naver.com>
2023-06-08 20:00:30 +0900

    action.js three import 추가

--- fairy-tale-book/src/scripts/animation/playAction.js
+++ fairy-tale-book/src/scripts/animation/playAction.js
@@ -1,24 +1,26 @@
+import * as THREE from "three";
+
 export const playAction = (clipAction, mixer) => {
-  const action = mixer.clipAction(clipAction);
-  action.setLoop(THREE.LoopOnce);
-  action.clampWhenFinished = true;
-  action.enabled = true;
-  action.paused = false;
-  action.timeScale = 1;
-  // reset시 필요
-  action.reset();
-  action.play();
+    const action = mixer.clipAction(clipAction);
+    action.setLoop(THREE.LoopOnce);
+    action.clampWhenFinished = true;
+    action.enabled = true;
+    action.paused = false;
+    action.timeScale = 1;
+    // reset시 필요
+    action.reset();
+    action.play();
 };
 
 export const playReverseAction = (clipAction, mixer) => {
-  let action = mixer.clipAction(clipAction);
-  action.paused = false;
-  action.time = action.getClip().duration;
-  action.timeScale = -1;
-  action.setLoop(THREE.LoopOnce);
-  action.clampWhenFinished = true;
-  action.enabled = true;
-  // reset시 필요
-  // action.reset();
-  action.play();
+    let action = mixer.clipAction(clipAction);
+    action.paused = false;
+    action.time = action.getClip().duration;
+    action.timeScale = -1;
+    action.setLoop(THREE.LoopOnce);
+    action.clampWhenFinished = true;
+    action.enabled = true;
+    // reset시 필요
+    // action.reset();
+    action.play();
 };
