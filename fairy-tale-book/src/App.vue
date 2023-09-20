3c64a41047e41488ed1a8f7d2b467f9319316b34
Leejaeyeop <dlwoduq78@naver.com>
2023-09-20 22:39:30 +0900

    book cover image 삭제 및 background 이미지 변경

--- fairy-tale-book/src/App.vue
+++ fairy-tale-book/src/App.vue
@@ -1,54 +1,58 @@
 <template>
-  <img class="background" src="background_sky.jpg" style="position: absolute" />
-  <Loading v-if="!isLoaded"></Loading>
-  <Main />
+    <img
+        class="background"
+        src="background.jpg"
+        style="position: absolute"
+    />
+    <Loading v-if="!isLoaded"></Loading>
+    <Main />
 </template>
 
 <script>
-import { computed } from 'vue';
-import Main from './components/Main.vue';
-import Loading from './components/Loading.vue';
-import { useStore } from 'vuex';
+import { computed } from "vue";
+import Main from "./components/Main.vue";
+import Loading from "./components/Loading.vue";
+import { useStore } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    Main,
-    Loading,
-  },
-  setup() {
-    const store = useStore();
-    const isLoaded = computed(() => {
-      return store.getters.initCompleted;
-    });
-    return {
-      isLoaded,
-    };
-  },
+    name: "App",
+    components: {
+        Main,
+        Loading,
+    },
+    setup() {
+        const store = useStore();
+        const isLoaded = computed(() => {
+            return store.getters.initCompleted;
+        });
+        return {
+            isLoaded,
+        };
+    },
 };
 </script>
 
 <style>
 html {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
 
 .background {
-  position: absolute;
-  height: 100%;
-  width: 100%;
+    position: absolute;
+    height: 100%;
+    width: 100%;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    /* margin-top: 60px; */
 }
 body {
-  margin: 0px;
+    margin: 0px;
 }
 </style>
